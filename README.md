38a2c2cbb46c6610ba07dcf3097721c01e6d37c7
Christian Gati <christian.gati@gmail.com>
2015-04-07 20:44:53 -0400

    Create base project and read config file
    Base project created. Initial project reads the API key from a
    config.json file.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Dell Lookup
+A soon-to-be web-interface for looking up warranty status based on Dell Service Tag
+
+### Configuration
+Currently the only configuration option is to provide the application an API Key for Dell's Service Tag system. This file is stored at the root of the project.
+
+config.json
+
+```
+{
+    "ApiKey": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+}
+```
---
+++ src/github.com/cgati/search/config.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+)
+
+type Configuration struct {
+	ApiKey string
+}
+
+func importConfig() string {
+	file, err := ioutil.ReadFile("../config.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	configuration := Configuration{}
+	err = json.Unmarshal(file, &configuration)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return configuration.ApiKey
+}
