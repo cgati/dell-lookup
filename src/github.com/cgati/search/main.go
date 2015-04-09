7f2d5a1b091d4f3a4862754a152ccf4c6be51ecd
Christian Gati <christian.gati@gmail.com>
2015-04-09 05:23:00 -0400

    Create Asset struct and complete regex on input
    Created an Asset struct in which we can move the wanted information out
    of the initial struct used for unmarshaling.
    Also began checking input for proper service tag formatting.

--- src/github.com/cgati/search/main.go
+++ src/github.com/cgati/search/main.go
@@ -3,25 +3,55 @@ package main
 import (
 	"fmt"
 	"os"
+	"regexp"
 )
+
+func printWarranty(assets []Asset) {
+	for _, asset := range assets {
+		fmt.Println(asset.MachineDescription + " -- " + asset.ServiceTag)
+		for _, warranty := range asset.Warranties {
+			fmt.Println(warranty.ServiceLevelDescription)
+			fmt.Println(warranty.StartDate + " -- " + warranty.EndDate + "\n")
+		}
+		fmt.Printf("\n\n")
+	}
+}
 
 func main() {
 	args := os.Args
-	//k33bz: Add error checking. REGEX: [0-9a-zA-Z]{7}
+	r := regexp.MustCompile("^[0-9a-zA-Z]{7}$")
+
 	if len(args) < 2 {
 		fmt.Println("Please provide at least one service tag.")
-		//k33bz: Updated to match documentation
 		fmt.Printf("Usage: %s 1A2B3C4", args[0])
-	} else {
-		content, err := searchServiceTags(args[1:])
-		if err != nil {
-			fmt.Printf("Sorry, an error occurred.")
-		} else {
-			if len(args) == 2 {
-				getWarrantyInformation(content, false)
-			} else {
-				getWarrantyInformation(content, true)
-			}
+		return
+	}
+	for _, arg := range args[1:] {
+		if r.MatchString(arg) == false {
+			fmt.Printf("One or more of your service tags was invalid. Please try again.")
+			return
 		}
 	}
+	content, err := searchServiceTags(args[1:])
+	if err != nil {
+		fmt.Printf("Sorry, an error occurred.")
+		return
+	}
+
+	dellAssets := []Asset{}
+	if len(args) == 2 {
+		dellAssets, err = getWarrantyInformation(content, false)
+		if err != nil {
+			fmt.Println("there was an error processing your service tag")
+			return
+		}
+	} else {
+		dellAssets, err = getWarrantyInformation(content, true)
+		if err != nil {
+			fmt.Println("there was an error processing your service tags")
+			return
+		}
+	}
+
+	printWarranty(dellAssets)
 }
--- src/github.com/cgati/search/unmarshal.go
+++ src/github.com/cgati/search/unmarshal.go
@@ -2,8 +2,7 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
-	"log"
+	"errors"
 )
 
 type DellWarrantySingle struct {
@@ -90,7 +89,105 @@ type DellWarrantyList struct {
 	} `json:"GetAssetWarrantyResponse"`
 }
 
-func getWarrantyInformation(content []byte, multiple bool) {
+type Asset struct {
+	CountryLookupCode  float64
+	CustomerNumber     float64
+	IsDuplicate        string
+	ItemClassCode      string
+	LocalChannel       float64
+	MachineDescription string
+	OrderNumber        float64
+	ParentServiceTag   string
+	ServiceTag         string
+	ShipDate           string
+	Warranties         []Warranty
+}
+
+type Warranty struct {
+	EndDate                 string
+	EntitlementType         string
+	ItemNumber              string
+	ServiceLevelCode        string
+	ServiceLevelDescription string
+	ServiceLevelGroup       float64
+	ServiceProvider         string
+	StartDate               string
+}
+
+func createDellAssetFromList(warranties DellWarrantyList) []Asset {
+	items := warranties.GetAssetWarrantyResponse.GetAssetWarrantyResult.Response.DellAsset
+	assetList := []Asset{}
+	for _, item := range items {
+		warranties := []Warranty{}
+		for _, warranty := range item.Warranties.Warranty {
+			w := Warranty{
+				StartDate:               warranty.StartDate,
+				EndDate:                 warranty.EndDate,
+				EntitlementType:         warranty.EntitlementType,
+				ItemNumber:              warranty.ItemNumber,
+				ServiceLevelCode:        warranty.ServiceLevelCode,
+				ServiceLevelDescription: warranty.ServiceLevelDescription,
+				ServiceLevelGroup:       warranty.ServiceLevelGroup,
+				ServiceProvider:         warranty.ServiceProvider,
+			}
+			warranties = append(warranties, w)
+		}
+		asset := Asset{
+			CountryLookupCode:  item.CountryLookupCode,
+			CustomerNumber:     item.CustomerNumber,
+			IsDuplicate:        item.IsDuplicate,
+			ItemClassCode:      item.ItemClassCode,
+			LocalChannel:       item.LocalChannel,
+			MachineDescription: item.MachineDescription,
+			OrderNumber:        item.OrderNumber,
+			ParentServiceTag:   item.ParentServiceTag._Nil,
+			ServiceTag:         item.ServiceTag,
+			ShipDate:           item.ShipDate,
+			Warranties:         warranties,
+		}
+		assetList = append(assetList, asset)
+	}
+	return assetList
+}
+
+// there has to be a better way...
+
+func createDellAssetFromSingle(warranties DellWarrantySingle) []Asset {
+	item := warranties.GetAssetWarrantyResponse.GetAssetWarrantyResult.Response.DellAsset
+	assetList := make([]Asset, 1)
+	warrantyList := make([]Warranty, len(item.Warranties.Warranty))
+	for _, warranty := range item.Warranties.Warranty {
+		w := Warranty{
+			StartDate:               warranty.StartDate,
+			EndDate:                 warranty.EndDate,
+			EntitlementType:         warranty.EntitlementType,
+			ItemNumber:              warranty.ItemNumber,
+			ServiceLevelCode:        warranty.ServiceLevelCode,
+			ServiceLevelDescription: warranty.ServiceLevelDescription,
+			ServiceLevelGroup:       warranty.ServiceLevelGroup,
+			ServiceProvider:         warranty.ServiceProvider,
+		}
+		warrantyList = append(warrantyList, w)
+	}
+	asset := Asset{
+		CountryLookupCode:  item.CountryLookupCode,
+		CustomerNumber:     item.CustomerNumber,
+		IsDuplicate:        item.IsDuplicate,
+		ItemClassCode:      item.ItemClassCode,
+		LocalChannel:       item.LocalChannel,
+		MachineDescription: item.MachineDescription,
+		OrderNumber:        item.OrderNumber,
+		ParentServiceTag:   item.ParentServiceTag._Nil,
+		ServiceTag:         item.ServiceTag,
+		ShipDate:           item.ShipDate,
+		Warranties:         warrantyList,
+	}
+	assetList = append(assetList, asset)
+	return assetList
+}
+
+func getWarrantyInformation(content []byte, multiple bool) ([]Asset, error) {
+	dellAssets := []Asset{}
 	// a lack of generics and not knowing better is causing this
 	// schism right now. look into a better solution.
 	// it also doesn't help that the API returns this subtle difference.
@@ -98,22 +195,16 @@ func getWarrantyInformation(content []byte, multiple bool) {
 		warranty := DellWarrantyList{}
 		err := json.Unmarshal(content, &warranty)
 		if err != nil {
-			log.Fatal(err)
+			return dellAssets, errors.New("failed to unmarshal")
 		}
-		assets := warranty.GetAssetWarrantyResponse.GetAssetWarrantyResult.Response.DellAsset
-		for _, asset := range assets {
-			fmt.Println(asset.MachineDescription + " - " + asset.ServiceTag)
-		}
+		dellAssets = createDellAssetFromList(warranty)
 	} else {
 		warranty := DellWarrantySingle{}
 		err := json.Unmarshal(content, &warranty)
 		if err != nil {
-			log.Fatal(err)
+			return dellAssets, errors.New("failed to unmarshal")
 		}
-		asset := warranty.GetAssetWarrantyResponse.GetAssetWarrantyResult.Response.DellAsset
-		fmt.Println(asset.MachineDescription + " - " + asset.ServiceTag)
+		dellAssets = createDellAssetFromSingle(warranty)
 	}
-
-	// quick and dirty testing
-	// will fix soon
+	return dellAssets, nil
 }
