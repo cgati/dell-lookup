b78be5548fb321fb27ca9994942119891ebab856
Christian Gati <christian.gati@gmail.com>
2015-04-20 21:14:07 -0400

    Forgot to commit main.js somehow

--- static/js/main.js
+++ static/js/main.js
@@ -25,8 +25,7 @@ function getServiceTag(sts) {
     request.onload = function() {
       if (this.status >= 200 && this.status < 400) {
         // Success!
-        var resp = this.response;
-        dump.textContent = resp;
+        renderResult(this.response);
       } else {
         // We reached our target server, but it returned an error
         dump.textContent = "An error occurred. Status: " + this.status;
@@ -40,4 +39,62 @@ function getServiceTag(sts) {
 
     request.send(sts);
 
+}
+
+function renderResult(data) {
+    var container = document.getElementById("container");
+    var rendered = document.getElementById("assetList");
+    if (rendered !== null) {
+        container.removeChild(rendered);
+    }
+    var dellResult = JSON.parse(data);
+    var assetList = document.createElement("div");
+    assetList.setAttribute("id", "assetList");
+    for (a of dellResult.Assets) {
+        var asset = document.createElement("div");
+        var assetSpan = document.createElement("span");
+        assetSpan.textContent = a.ServiceTag + " - " + a.MachineDescription;
+        asset.appendChild(assetSpan);
+        var wTable = document.createElement("table");
+        var tHead = document.createElement("thead");
+        var tHeadRow = document.createElement("tr");
+        var thDescription = document.createElement("th");
+        thDescription.textContent = "Description";
+        var thStart = document.createElement("th");
+        thStart.textContent = "Start Date";
+        var thEnd = document.createElement("th");
+        thEnd.textContent = "End Date";
+        var thInWarranty = document.createElement("th");
+        thInWarranty.textContent = "Is In Warranty?";
+        tHeadRow.appendChild(thDescription);
+        tHeadRow.appendChild(thStart);
+        tHeadRow.appendChild(thEnd);
+        tHeadRow.appendChild(thInWarranty);
+        tHead.appendChild(tHeadRow);
+        wTable.appendChild(tHead);
+        var tBody = document.createElement("tbody");
+        for (w of a.Warranties.Warranty) {
+            var tBodyRow = document.createElement("tr");
+            var tdDescription = document.createElement("td");
+            tdDescription.textContent = w.ServiceLevelDescription;
+            var tdStartDate = document.createElement("td");
+            tdStartDate.textContent = w.StartDate;
+            var tdEndDate = document.createElement("td");
+            tdEndDate.textContent = w.EndDate;
+            var tdInWarranty = document.createElement("td");
+            tdInWarranty.textContent = w.InWarranty ? "Yes." : "No.";
+            tBodyRow.appendChild(tdDescription);
+            tBodyRow.appendChild(tdStartDate);
+            tBodyRow.appendChild(tdEndDate);
+            tBodyRow.appendChild(tdInWarranty);
+            tBody.appendChild(tBodyRow);
+        }
+        wTable.appendChild(tBody);
+        asset.appendChild(wTable);
+        assetList.appendChild(asset);
+        var brk = document.createElement("br");
+        assetList.appendChild(brk);
+    }
+    var dump = document.getElementById("content");
+    container.insertBefore(assetList, dump.nextSibling);
 }
